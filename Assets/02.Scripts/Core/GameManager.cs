01470ab94cb81ccd46b2c964401150d9c32ee3b3
khh7052 <59150989+khh7052@users.noreply.github.com>
2025-08-12 19:27:46 +0900

    [Feat] 리스폰 기능 추가

---
+++ Assets/02.Scripts/Core/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] private Transform respawnPoint;
+    [SerializeField] private float respawnHeight = -10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HasFallenBelowRespawnHeight())
+            Respawn();
+    }
+
+    bool HasFallenBelowRespawnHeight() => playerController.transform.position.y <= respawnHeight;
+
+    public void SetRespawnPoint(Transform newRespawn) => respawnPoint = newRespawn;
+
+    void Respawn()
+    {
+        if (playerController == null || respawnPoint == null) return;
+
+        playerController.transform.position = respawnPoint.position;
+        playerController.transform.rotation = respawnPoint.rotation;
+    }
+}
